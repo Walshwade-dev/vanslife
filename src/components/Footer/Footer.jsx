a4060e364329a670fa0842584c9c4f69ceabf176
Walshwade-dev <mainace8@gmail.com>
2023-08-15 08:55:14 +0300

    third commit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
     let date = new Date().getFullYear();
 
     return(
-        <footer className="fixed h-20 bottom-0 lg:bottom-0 bg-bkg text-content flex justify-center items-center lg:h-20 w-full">
+        <footer className="fixed h-10 bottom-0 lg:bottom-0 bg-bkg text-content flex justify-center items-center lg:h-20 w-full">
             <p className="text-sm tracking-wide"> © {date} #VANLIFE</p> 
         </footer>
     )
